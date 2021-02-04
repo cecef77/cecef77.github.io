2e215f590bffb69a4d7cd576cbc7a6f64b62f341
arembo <cecef77@gmail.com>
2021-02-04 18:02:59 +0700

    portofolio

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// event saat di klit
+$('.page').on('click', function(e){
+
+ // ambil
+ var tujuan = $(this).attr('href');
+//tankap eleme
+ var elemenTujuan = $(tujuan);
+    $('body').animate({
+         elemenTujuan.offset().top - 50
+    }, 1000'swing');
+
+  e.preventDefault();
+scrolltop
+
+});
