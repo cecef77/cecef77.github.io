c54c0df666a5c9e7178a49eeaa0b71d176ad1a99
cecef77 <cecef77@gmail.com>
2021-02-17 17:50:33 +0700

    add paralax

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+/* ------------------------------- // SECTION ------------------------------- */
+
+/* ------------------------------ //NOTE easing ----------------------------- */
+
 $('.page-scroll').on('click', function(e) {
 
    var tujuan = $(this).attr('href');
@@ -10,3 +14,59 @@ $('.page-scroll').on('click', function(e) {
   
    e.preventDefault();
   });
+
+/* ------------------------------- // SECTION ------------------------------- */
+
+/* ----------------------------- // NOTE paralax ---------------------------- */
+
+/* -------------- ------------------- SECTION -------------------------------- */
+
+/* -------------------------- NOTE paralax paragrap ------------------------- */
+$(window).on('load',function(){
+   $('.pkiri').addClass('pmuncul');
+   $('.pkanan').addClass('pmuncul');
+});
+
+
+
+
+
+
+
+
+/* --------------------------------- SECTION -------------------------------- */
+
+/* --------------------- NOTE 01.paralax untuk jumboton --------------------- */
+$(window).scroll(function(){
+   var wScroll = $(this).scrollTop();
+   $('.jumbotron img').css({  
+      'transform': 'translate(0px,'+wScroll/4+'% )'});
+   $('.jumbotron h1').css({
+      'transform': 'translate(0px,'+wScroll/2+'% )'});
+
+   $('.jumbotron p').css({
+      'transform': 'translate(0px,'+wScroll/1+'% )'});
+   
+
+
+/* ---------------------- NOTE 2.paralax untuk portofolio --------------------- */
+if(wScroll > $('.portofolio').offset().top - 200){
+   $('.portofolio .thumbnail').each(function(i){
+      setTimeout(function(){
+         $('.portofolio .thumbnail').eq(i).addClass('muncul');
+      }, 300 * (i+1));
+   });
+}
+
+})
+
+
+
+
+
+
+
+
+
+
+
